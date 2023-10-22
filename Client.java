1557b6a6f0751d9024dad7a9b9f3381c3843ff2e
AriTJ <Ari.Joh20@student.setur.fo>
2023-10-22 17:28:19 +0200

    Added some extra examples to Client.java

--- Client.java
+++ Client.java
@@ -6,10 +6,19 @@ import java.rmi.RemoteException;
 public class Client {
     public static void main(String[] args) throws MalformedURLException, RemoteException, NotBoundException {
         IPrintServer printServer = (IPrintServer) Naming.lookup("rmi://localhost:5099/printer");
-        String printer = "PrintServer";
+        String printer = "PrintServer"; // <-- part of assignment specification (don't know the use-case..)
 
+        // Example use:
+        System.out.println(printServer.status(printer));
         printServer.start();
+        System.out.println(printServer.status(printer));
+        printServer.topQueue(printer, 100);
+        printServer.topQueue(printer, 200);
+        printServer.topQueue(printer, 300);
+        System.out.println(printServer.queue(printer));
+        printServer.print("test.txt", printer);
         printServer.print("test2.txt", printer);
-        printServer.stop();
+        
+        // TODO: Create UI for Client?
     }
 }
